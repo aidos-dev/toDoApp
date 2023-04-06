0c6cb99840b4d0232a41912419a7ed70fe1805c5
Optimus <3300532@gmail.com>
2023-04-06 16:27:22 +0600

    dependency injection

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	todo "github.com/aidos-dev/toDoApp"
 	"github.com/aidos-dev/toDoApp/pkg/handler"
+	"github.com/aidos-dev/toDoApp/pkg/repository"
+	"github.com/aidos-dev/toDoApp/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	// handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
