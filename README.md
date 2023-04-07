20516a64d671b72978e49f22ba448bf0ec21c9fe
Optimus <3300532@gmail.com>
2023-04-07 18:47:33 +0600

    data base

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # toDoList
+
+### DataBase
+
+In this project a PostgreSQL data base is applied. In order to start data base from docker container it is required to download a PostgreSQL docker image with the command:
+
+```
+docker pull postgres
+```
--- cmd/main.go
+++ cmd/main.go
@@ -7,17 +7,45 @@ import (
 	"github.com/aidos-dev/toDoApp/pkg/handler"
 	"github.com/aidos-dev/toDoApp/pkg/repository"
 	"github.com/aidos-dev/toDoApp/pkg/service"
+
+	_ "github.com/lib/pq"
+	"github.com/spf13/viper"
 )
 
 func main() {
-	// handlers := new(handler.Handler)
-	repos := repository.NewRepository()
+	if err := initConfig(); err != nil {
+		log.Printf("error occured while running initConfig: %s", err.Error())
+		return
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		Host:     "localhost",
+		Port:     "5432",
+		Username: "postgres",
+		Password: "qwerty",
+		DBName:   "postgres",
+		SSLMode:  "disable",
+	})
+	if err != nil {
+		log.Printf("failed to initialize db: %s", err.Error())
+		return
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
-		log.Println(err.Error())
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Printf("error occured while running http server: %s", err.Error())
 		return
 	}
+}
+
+func initConfig() error {
+	// AddConfigPath receives a derectory name
+	viper.AddConfigPath("configs")
+	// SetConfig receives a file name (from the directory above)
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
