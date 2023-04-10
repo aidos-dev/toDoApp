05ae753c9d91446ae006e93749b2e9b3f0ce03e4
Optimus <3300532@gmail.com>
2023-04-10 12:16:34 +0600

    user authorization

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -1,18 +1,31 @@
 package service
 
 import (
+	"crypto/sha1"
+	"fmt"
+
 	todo "github.com/aidos-dev/toDoApp"
 	"github.com/aidos-dev/toDoApp/pkg/repository"
 )
 
+const salt = "lk65vm29vkf437fb817hfn3857kdn4nv"
+
 type AuthService struct {
-	repo repository.Repository
+	repo repository.Authorization
 }
 
-func NewAuthService(repo repository.Repository) *AuthService {
+func NewAuthService(repo repository.Authorization) *AuthService {
 	return &AuthService{repo: repo}
 }
 
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
+	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user)
+}
+
+func generatePasswordHash(password string) string {
+	hash := sha1.New()
+	hash.Write([]byte(password))
+
+	return fmt.Sprintf("%x", hash.Sum([]byte(salt)))
 }
